9bde73b58349ed7dcd3177e86c1d6103bf2f6e4e
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-05-19 21:03:23 +0300

    Add files via upload

---
+++ homework_2.1.py
@@ -0,0 +1,6 @@
+# Переменные 2-ая задача
+count_home_tasks = 12
+count_hour = 1.5
+course_name = 'Python'
+time_one_task = count_hour / count_home_tasks
+print(f' Курс: {course_name} , всего задач: {count_home_tasks}, затрачено часов: {count_hour}, среднее время выполнения {time_one_task} часа.')
